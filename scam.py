4746649d4eee84ed3a510143cf4cd4858ccc4e3b
torgodly <57685643+torgodly@users.noreply.github.com>
2020-02-04 04:19:55 -0500

    Update scam.py

--- scam.py
+++ scam.py
@@ -7,6 +7,7 @@ import json
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
+#change the url to the request usel of the scam page
 url = 'https://www.bcip.ac.in/cgi-sys/js/simple-expand.min.js'
 
 names = json.loads(open('names.json').read())
@@ -19,9 +20,12 @@ for name in names:
 
     requests.post(url, allow_redirects=False, data={
 
+       #change thes 2 to the form data 
          "l3onwen": username,
          "lbass": password
 
     })
 
     print ("sending username %s and password %s" % (username, password))
+    
+    # you can find the request url and the form data in the network sction in inspact page 
